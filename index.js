39f969fa66a8f3ebc9cfbf1b0d2aa58ce008d6fa
Mahmoud Alshahin <mahmoudshaheen198811@gmail.com>
2023-03-20 23:58:48 +0300

    Done Challeng

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+   return (`This Saturday, I want to ${activity}!`) 
+  }
+  const wrapAdjective = function (action = 'go to the office'){
+    return function (spa="special"){
+       return (`You are ${action}${spa}${action}!`)
+    }
+}
+
+  function mondayWork(ar='go to the office'){
+    return (`This Monday, I will ${ar}.`)
+  }
